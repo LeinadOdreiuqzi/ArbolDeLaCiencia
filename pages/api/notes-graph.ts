7ef0448aa324fb55be82076592fe09a4d76d8399
LeinadOdreiuqzi <sherdanialex159357@gmail.com>
2025-04-27 18:17:52 -0500

    unnecessary file

--- pages/api/notes-graph.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import path from "path";
-import { buildNotesTree } from "@/lib/notes-graph-util";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const notesDir = path.join(process.cwd(), "app", "notes");
-  const tree = buildNotesTree(notesDir);
-  res.status(200).json(tree);
-}
